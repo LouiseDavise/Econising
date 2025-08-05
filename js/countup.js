0bccbb2d0fa917eac61521081a09768629f1d6e5
LouiseDavise <louizkwok@gmail.com>
2025-08-06 02:07:49 +0800

    Initial Commit

---
+++ js/countup.js
@@ -0,0 +1,12 @@
+// let negara = 0
+// document.getElementById('negara').innerHTML = negara + "+"
+// while (true) {
+//     document.querySelector('negara').innerHTML = negara + "+"
+
+//     if (negara < 20) {
+//         setTimeout(() => negara + 1, 60)
+//     }
+//     else{
+//         break
+//     }
+// }
